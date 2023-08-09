89462942e0dee967e0e2abf4d02b366512fb6de9
abhishekksingh02 <abhisingh8734@gmail.com>
2023-08-09 12:59:15 +0530

    add

--- CC++/primenotprime.c
+++ CC++/primenotprime.c
@@ -4,14 +4,14 @@ int main()
     int n,count=0,i;
     printf("Enter any number:");
     scanf("%d",&n);
-    for(i=0;i<=n;i++)
+    for(i=2;i<n;i++)
      {
-        if(n%1==0)
+        if(n%i == 0)
         {
-            count++;
+            count=1;
         }
      }
-     if(count==2)
+     if(count==0)
       printf("Prime number");
       else
         printf("Not a prime number");
--- CC++/total_percentage.c
+++ CC++/total_percentage.c
@@ -1,23 +1,42 @@
-#include<stdio.h>
-#include<conio.h>
- void main()
- { 
-    int a,b,c,d,e,total;
-    float percentage;
-      printf("Enter the marks of first subject:");
-      scanf("%d",&a);
-       printf("Enter the marks of second subject:");
-       scanf("%d",&b);
-        printf("Enter the marks of third subject:");
-        scanf("%d",&c);
-         printf("Enter the marks of fourth subject:");
-         scanf("%d",&d);
-           printf("Enter the marks of fifth subject:");
-           scanf("%d",&e);
-      total=a+b+c+d+e;
-      printf("total marks %d", total);
-       percentage= total/5*100;
-         printf("percentage: %f",percentage);
-       getch();      
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+  int a, b, c, d, e, total;
+  float percentage;
+  printf("Enter the marks of first subject:");
+  scanf("%d", &a);
+  printf("Enter the marks of second subject:");
+  scanf("%d", &b);
+  printf("Enter the marks of third subject:");
+  scanf("%d", &c);
+  printf("Enter the marks of fourth subject:");
+  scanf("%d", &d);
+  printf("Enter the marks of fifth subject:");
+  scanf("%d", &e);
+  total = a + b + c + d + e;
+  printf("\ntotal marks %d", total);
+  if (a <= 28 || b <= 28 || c <= 28 || d <= 28 || e <= 28)
+  {
 
- }
+    printf("\nYOU ARE FAIL.\n");
+  }
+  else
+  {
+    percentage = total / 5;
+    printf("percentage: %f", percentage);
+    if (percentage > 75)
+    {
+      printf("PASSED WITH DISTINCTION:");
+    }
+    else if (percentage > 60 && percentage <= 75)
+      printf("PASSED WITH FIRST CLASS:");
+    else if (percentage > 50 && percentage <= 60)
+      printf("PASSED WITH SECOND CLASS:");
+    else if (percentage > 35 && percentage <= 50)
+      printf("PASSED WITH PASS CLASS:");
+
+    else
+      printf("SORRY APPLY FOR REASSESSMENT:");
+  }
+}
