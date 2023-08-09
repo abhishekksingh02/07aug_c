9f2987d3931d7981f00094582d91e0e379e08a77
abhishekksingh02 <abhisingh8734@gmail.com>
2023-08-09 17:32:29 +0530

    add

---
+++ CC++/squarecube.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{ 
+     int n,square,cube;
+     printf("Enter any nymber:");
+     scanf("%d",&n);
+     square=n*n;
+     cube=n*n*n;
+      printf("Square of a number=%d",square);
+      printf("Cube of a number=%d",cube);
+ return 0;
+}
