d780d709853786eff7aa6539f3d6a637062a42c4
abhishekksingh02 <abhisingh8734@gmail.com>
2023-08-11 18:16:39 +0530

    add

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,23 +1,14 @@
 #include<stdio.h>
-#include<conio.h>
- void main()
+int main()
  { 
-    int a,b,c,d,e,total;
-    float percentage;
-      printf("Enter the marks of first subject:");
-      scanf("%d",&a);
-       printf("Enter the marks of second subject:");
-       scanf("%d",&b);
-        printf("Enter the marks of third subject:");
-        scanf("%d",&c);
-         printf("Enter the marks of fourth subject:");
-         scanf("%d",&d);
-           printf("Enter the marks of fifth subject:");
-           scanf("%d",&e);
-      total=a+b+c+d+e;
-      printf("total marks %d", total);
-       percentage= total/5*100;
-         printf("percentage: %f",percentage);
-       getch();      
-
+    int a,b;
+     printf("Enter the value of a and b:");
+     scanf("%d%d",&a,&b);
+      if(a>b)
+      {
+        printf("a is max:");
+      }
+    else
+      printf("b is max:");
+ return 0;
  }
