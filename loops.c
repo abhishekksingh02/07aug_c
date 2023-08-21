a6b1caa8e1cd353f43c72372ec6fab7dcf4af0f6
abhishekksingh02 <abhisingh8734@gmail.com>
2023-08-21 17:11:19 +0530

    add

--- loops.c
+++ loops.c
@@ -2,11 +2,12 @@
 int main()
 {
     int i,j;
-     for(i=1;i<=3;i++)
-      {
-        for(j=1;j<=5;j++)
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
         {
             printf("* ");
         }
-      } printf("\n");
+        printf("\n");
+    }
 }
---
+++ multiplication.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{   float a,b,multiplication;
+    printf("Enter your first number:");
+    scanf("%f",&a);
+     printf("Enter your second number:");
+     scanf("%f",&b);
+    multiplication=a*b;
+     printf("Multiplication=%f:",multiplication);
+
+}
